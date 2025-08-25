0b353b7325cc89d65da0ad840dff768acd49669b
daydream-skybreak <samueldesta323@gmail.com>
2025-08-25 15:02:06 +0300

    feat: adds home page

--- mvc_project/Controllers/HomeController.cs
+++ mvc_project/Controllers/HomeController.cs
@@ -1,21 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using mvc_project.Models;
+using mvc_project.Services;
 
 namespace mvc_project.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly JsonDataService _jsonData;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,  JsonDataService jsonData)
     {
         _logger = logger;
+        _jsonData = jsonData;
     }
 
     public IActionResult Index()
     {
-        return View();
+        return View(_jsonData);
     }
 
     public IActionResult Privacy()
