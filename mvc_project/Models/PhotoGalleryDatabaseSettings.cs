f062905acef96aafcc4f348d6b604f15fb1e80cd
daydream-skybreak <samueldesta323@gmail.com>
2025-09-08 16:34:07 +0300

    addes get function to the photos service

---
+++ mvc_project/Models/PhotoGalleryDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace mvc_project.Models;
+
+public class PhotoGalleryDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string BooksCollectionName { get; set; } = null!;
+}
---
+++ mvc_project/Services/PhotoService.cs
@@ -0,0 +1,20 @@
+﻿using mvc_project.Models;
+
+namespace mvc_project.Services;
+using MongoDB.Driver;
+
+public class PhotoService
+{
+    private readonly IMongoCollection<Photo> _photos;
+
+    public PhotoService(IConfiguration config)
+    {
+        var  client = new MongoClient(config.GetConnectionString("MongoDB"));
+        var database = client.GetDatabase(config.GetConnectionString("MongoDB"));
+        _photos = database.GetCollection<Photo>(config.GetConnectionString("PhotoCollection"));
+    }
+    
+    public List<Photo> GetAll() => _photos.Find(photo => true).ToList();
+    
+    public async Task<List<Photo>> GetByBookId(string photoId) => await _photos.Find( id => id.Id == photoId).ToListAsync();
+}
