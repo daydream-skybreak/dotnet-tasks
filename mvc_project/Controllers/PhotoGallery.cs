978c22c2d0dcd48bc08f2919674e413a4024bee8
daydream-skybreak <samueldesta323@gmail.com>
2025-09-08 16:21:50 +0300

    mongodb model created

---
+++ mvc_project/Controllers/PhotoGallery.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace mvc_project.Controllers;
+
+public class PhotoGallery : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ mvc_project/Models/Photo.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace mvc_project.Models;
+
+public class Photo
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    private string path { get; set; }
+}
